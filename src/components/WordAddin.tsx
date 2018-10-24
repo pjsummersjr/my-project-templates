f7ce81bd8d860d411a18b1ea7dbbc440c7096eb9
Paul Summers <paulsumm@microsoft.com>
2018-10-24 12:11:10 -0400

    Retrieving data and rendering in list format

--- src/components/WordAddin.tsx
+++ src/components/WordAddin.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { ProgressIndicator } from 'office-ui-fabric-react/lib/ProgressIndicator';
+import { DocumentCard, DocumentCardTitle } from 'office-ui-fabric-react/lib/DocumentCard';
 
 import * as OfficeHelpers from '@microsoft/office-js-helpers';
  
 import { IOfficeAddinProps } from './OfficeAddin';
+import { access } from 'fs';
 
 export interface IWordAddinProps extends IOfficeAddinProps {
 
@@ -34,29 +36,32 @@ export default class WordAddin extends React.Component<IWordAddinProps, IWordAdd
     loadContent = () => {
         let self = this;
 
-        let resourceUrl:string = "https://pjsummersjr2.ngrok.io/api/opportunities";
+        let resourceUrl:string = "https://localhost:3001/api/opportunities";
         self.props.authenticator.authenticate(this.provider, false).then(
             function(response: any){
                 console.debug(`Requesting data from ${resourceUrl}`);
                 let accessToken = response.access_token;
-                var request = new XMLHttpRequest();
-                request.addEventListener('load', () => {
-                    console.log(`Response from server: ${JSON.stringify(this.responseText)}`);
-                    self.setState({
-                        data:this.responseText
-                    })
-                });
-                request.open('GET', resourceUrl);
-                request.setRequestHeader("Authorization", "Bearer " + accessToken);
-                request.setRequestHeader("Content-Type", "application/json");
-                request.setRequestHeader("Accept", "application/json");
-                request.setRequestHeader("Cache-Control", "no-cache");
-                request.send();
-            },
-            function(error: any) {
-                console.log(`Error from authenticate: ${error}`);
-                var token = self.props.authenticator.tokens.get(self.provider);
-                console.log(`Got a token: ${JSON.stringify(token)}`);
+                let requestConfig = {
+                    method:"GET",
+                    headers: {
+                        "Authorization": `Bearer ${accessToken}`,
+                        "Content-Type": `application/json`                    }
+                }
+                fetch(resourceUrl, requestConfig)
+                .then(response => response.json()) //Not quite sure why the multiple steps are required
+                .then(
+                    function(response){
+                        //console.log(JSON.stringify(response));
+                        self.setState({
+                            data:response
+                        });
+                    },
+                    (error) => {
+                        console.log(`Error from authenticate: ${error}`);
+                        var token = self.props.authenticator.tokens.get(self.provider);
+                        console.log(`Got a token: ${JSON.stringify(token)}`);
+                    }
+                )
             }
         )
         .catch(function(error:any){
@@ -64,11 +69,24 @@ export default class WordAddin extends React.Component<IWordAddinProps, IWordAdd
         });
     }
 
+    drawOpportunities(oppData: any): any {
+        let oppContent = (<div>No opportunity data available</div>);
+        if(!oppData) return oppContent;
+        oppContent = oppData.value.map((item: any, index: number) =>{
+            return (<DocumentCard key={item.opportunityid}>
+                        <DocumentCardTitle title={item.name} shouldTruncate={false} />
+                        <DocumentCardTitle title={item.description ? item.description : 'No description found'} shouldTruncate={true} showAsSecondaryTitle={true} />
+                    </DocumentCard>)
+        })
+        return (oppContent);
+    }
+
     render() {
         if(!this.props.isOfficeInitialized) {
             return (<ProgressIndicator label="No Office environment detected" description="Please load this page within an Office add-in"/>);
         }
-        return (<div>{JSON.stringify(this.state.data)}</div>);
+        let opportunities: any = this.drawOpportunities(this.state.data);
+        return (<div>{opportunities}</div>);
     }
 
 }
