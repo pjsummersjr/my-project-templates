6c5d2b77ea1796e347f709fc27c07cbc9a91d52d
Paul Summers <paulsumm@microsoft.com>
2018-10-24 15:38:53 -0400

    Some cleanup and in-line documentation

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,10 @@ import * as ReactDOM from "react-dom";
 import SimpleService from './services/SimpleService';
 import WordAddin from './components/WordAddin';
 import * as OfficeHelpers from '@microsoft/office-js-helpers';
-//import {Authenticator} from '@microsoft/office-js-helpers';
 
 import {BaseConfig, tenantConfig } from './config/app.config';
 
 import './index.scss';
-import { IAddInConfig } from './components/OfficeAddin';
-
-let HelloWorld = () => {
-  let service: SimpleService = new SimpleService();
-  let message: string = service.getSimpleData()[0];
-  return (<h1>Hello there {message}! Welcome to the React World with Webpack!</h1>);
-}
 
 const render = (isOfficeInitialized: boolean, authProvider: OfficeHelpers.Authenticator) => {
   
@@ -35,7 +27,9 @@ Office.initialize = () => {
   
   render(true, authenticator);
 }
-
+/**
+ * We call this so something loads in the page if the page is loaded outside of an Office Add-In.
+ */
 render(false, null);
 
 
